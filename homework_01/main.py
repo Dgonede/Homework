f581e49d053429e15c4c3a3fe1d49c3a038f5639
Dgonede <dgonegonee@gmail.com>
2024-05-01 20:25:53 +0300

    Home

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,32 +1,40 @@
 # Возврат квадрата чисел
 def power_numbers(*args):
       return [number*number for number in args]
+
+
+# Нечетные числа
+num = []
+def odds(*args):
+    for num in args:
+        if num % 2 == 1: return True
+        if num % 2 == 0: return False
+ODD = odds
+
+# Четные числа
+nums = []
+def evens(*args):
+    for evens in args:
+        if evens % 2 == 1: return False
+        if evens % 2 == 0: return True
+EVEN = evens
+
+def prime(num):
+    if num == 1:return False
+    nums = True
+    i = num - 1
+    while i > 1:
+        if not num % i:
+            nums = False
+            break
+        i -= 1
+    return num if nums else False
+
+PRIME = prime
+
+def filter_numbers(a,b):
+    return list(filter(b,a))
 print(power_numbers(1, 2, 5, 7))
-
-
-#Проверка простого числа
-def is_prime(num):
-    n = num
-    count = 0
-    for i in range(1, n + 1):
-        if n % i == 0:
-            count +=1
-    return f"{num}  True " if count == 2 else f"{num} False"
-
-# Вывод четных, нечетных, просых чисел
-def filter_numbers(*args):
-    ODD = [
-        num for num in args
-        if num % 2 == 1
-    ]
-    print(ODD)
-    EVEN = [
-        num for num in args
-        if num % 2 == 0
-    ]
-    print(EVEN)
-    PRIME = [ 
-        is_prime(num) for num in args
-    ]
-    print(PRIME)
-filter_numbers(2, 1, 3, 5, 4) 
+print(filter_numbers([1, 2, 3], ODD)) 
+print(filter_numbers([2, 3, 4, 5], EVEN))
+print(filter_numbers([47,49, 54, 2, 5], PRIME))
