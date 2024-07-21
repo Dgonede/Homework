81b26c6ae3ccbe1ea52aadc4b6f7bbbe003d56ed
Dgonede <dgonegonee@gmail.com>
2024-07-21 10:27:19 +0300

    1

--- homework_04/main.py
+++ homework_04/main.py
@@ -95,7 +95,7 @@ async def fetch_users_with_posts(
             # joinedload(User.posts),
             selectinload(User.posts),
         )
-        .order_by(User)
+        .order_by(User.username)
     )
 
     print("load users w/ posts:")
@@ -105,7 +105,6 @@ async def fetch_users_with_posts(
         print("+", user)
         for post in user.posts:
             print("  -", post)
-            print(" +", users)
 
     return users
 
