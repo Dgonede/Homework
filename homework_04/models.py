336098421cb3c03b89c17ae609a7ea5c6de6c3d9
Dgonede <dgonegonee@gmail.com>
2024-07-21 18:53:45 +0300

    1

--- homework_04/models.py
+++ homework_04/models.py
@@ -11,13 +11,15 @@ from sqlalchemy import ForeignKey
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
+
 
 PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
 
 
 async_engine = create_async_engine(PG_CONN_URI)
 
+Session = sessionmaker(bind=async_engine)
 
 async_session = async_sessionmaker(
     bind=async_engine,
