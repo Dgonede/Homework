f84a43026fe2cd702bcd36028178534b8148f182
Dgonede <dgonegonee@gmail.com>
2024-07-21 10:13:28 +0300

    1

--- homework_04/main.py
+++ homework_04/main.py
@@ -123,7 +123,7 @@ async def fetch_all_posts_with_authors(
     stmt = (
         select(Post)
         .options(
-            selectinload(Post.author),
+            joinedload(Post.author),
         )
         .order_by(Post.id)
     )
