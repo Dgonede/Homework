ac0629e81747bb208231c82a59effd3da1056805
Dgonede <dgonegonee@gmail.com>
2024-07-29 20:35:52 +0300

    1

--- homework_04/main.py
+++ homework_04/main.py
@@ -76,11 +76,9 @@ async def async_main():
             title="MySQL news",
             user_id=john_user.id,
         )
-        
-        await asyncio.gather(
-        fetch_all_posts_with_authors(session)
-        )
-        
+        task = asyncio.create_task(fetch_all_posts_with_authors())
+        await asyncio.gather(task)
+    
         
     
  
